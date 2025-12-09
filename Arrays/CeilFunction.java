2ec0ac09fd278cce47cc3804532f276299548f80
johnthebrat7 <168217059+johnthebrat7@users.noreply.github.com>
2025-12-09 17:30:23 +0530

    Create CeilFunction.java

---
+++ Arrays/CeilFunction.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ArrayCeiling {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Enter target:");
+        int t = S.nextInt();
+        int[] a = {12, 33, 45, 66, 78, 98, 100};
+        System.out.println(binary(a, t));
+
+    }
+
+    static int binary(int[] a, int t) {
+        int s = 0;
+
+        int f = a.length - 1;
+        if(t>a[a.length-1]){
+            return -1;
+        }
+
+        while (f >= s) {
+            int mid = s + (f - s) / 2;
+            if (a[mid] > t) {
+                f = mid - 1;
+            } else if (a[mid] < t) {
+                s = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return a[s];
+    }
+}
