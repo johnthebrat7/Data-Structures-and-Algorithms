3cd9d22c4dc0f72a3cd6e36e3f42caa2e8c3f593
johnthebrat7 <168217059+johnthebrat7@users.noreply.github.com>
2026-01-12 22:35:53 +0530

    Create LC102.java
    102. Binary Tree Level Order Traversal  
    
    Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).

---
+++ Trees/LC102.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null){
+            return result;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer> list=new ArrayList<>();
+            int size=queue.size();
+            for(int i=0;i<size;i++){
+                TreeNode currentNode=queue.poll();
+                list.add(currentNode.val);
+                if(currentNode.left!=null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right!=null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
